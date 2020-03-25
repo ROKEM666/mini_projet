03e060aae5da7f556dc004259b04ba97be9e6c12
Chris-pc <cb25330@hotmail.fr>
2020-03-25 09:23:22 +0100

    test branche 2

---
+++ cmake-build-debug/CMakeFiles/mini_projet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mini_projet.dir/main.cpp.obj"
+  "mini_projet.exe"
+  "mini_projet.exe.manifest"
+  "mini_projet.lib"
+  "mini_projet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mini_projet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
