39e5189deee608397b8f860019b7b606b557bbcc
Darja38 <jasonlestage@orange.fr>
2020-03-25 09:10:14 +0100

    test jason

---
+++ cmake-build-debug/CMakeFiles/lancer_de.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lancer_de.dir/main.cpp.obj"
+  "lancer_de.pdb"
+  "lancer_de.exe"
+  "lancer_de.exe.manifest"
+  "liblancer_de.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lancer_de.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
